2f789d3b496629e64ee6ea522170d1f4b57bbf73
Dogus Cendek <doguscendek@gelecekrobotik.com>
2023-04-11 19:42:56 +0300

    Update Codes
    Update Codes

--- HC-SR04wLEDsBuzzer/HC-SR04wLEDsBuzzer.ino
+++ HC-SR04wLEDsBuzzer/HC-SR04wLEDsBuzzer.ino
@@ -1,18 +1,42 @@
+/* HC-SR04wLEDsBuzzer
+
+#  Maintainer & Author
+Maintainer: Turkish Technology Team (T3) Foundation (https://deneyapkart.org/)
+Author: Dogus Cendek (https://github.com/DogushC)
+
+#  Description
+Measure distance and show in Serial Monitor. Depends on distance, turn on/off LEDs and/or buzzer.
+
+#  Hardware Components
+1 x Deneyap Kart
+1 x HC-SR04 Ultrasonic Distance Sensor Module
+1 x Red LED
+1 x Yellow LED
+1 x Green LED
+1 x Buzzer
+4 x Resistor (100 ohm)
+1 x Breadboard
+1 x Micro-B USB Cable
+
+#  Library
+Add "HCSR04" Library by Martin Sosic (https://github.com/Martinsos/arduino-lib-hc-sr04)
+*/
+
 #include <HCSR04.h> //HC-SR04 lib
 
 /* Pins for HC-SR04 */
 #define trigPin D0
 #define echoPin D1
 
-UltraSonicDistanceSensor distanceSensor(trigPin, echoPin);  //HC-SR04 Config
+UltraSonicDistanceSensor distanceSensor(trigPin, echoPin);  //HC-SR04 config
 
-/* Pins for LEDs and Buzzer */
+/* Pins for LEDs and buzzer */
 const int buzzer = D12; //buzzer
 const int rLed = D13;  //red LED
 const int yLed = D14;  //yellow LED
 const int gLed = D15; //green LED
 
-float distance; //Variable for Distance
+float distance; //variable for distance
 
 void setup() {
   pinMode(rLed, OUTPUT); //set the rLed pin as output
@@ -25,9 +49,9 @@ void setup() {
 void loop() {
   distance = distanceSensor.measureDistanceCm(); //measure distance as centimeter
   Serial.print("Distance: ");
-  Serial.println(distance); //print distance
+  Serial.println(distance); //show the distance in Serial Monitor
   delay(100);
-
+  /* Depends on distance turn on/off LEDs and buzzer. */
   if (distance <= 25 && distance > 0) {
   digitalWrite(buzzer, HIGH);
   digitalWrite(rLed, HIGH);
---
+++ LDRwLED/LDRwLED.ino
@@ -0,0 +1,35 @@
+/* LDRwLED
+
+#  Maintainer & Author
+Maintainer: Turkish Technology Team (T3) Foundation (https://deneyapkart.org/)
+Author: Dogus Cendek (https://github.com/DogushC)
+
+#  Description
+Measure light intensity and show in Serial Monitor. Depends on the light intensity, adjust the brightness of the LED.
+
+#  Hardware Components
+1 x Deneyap Kart
+1 x LDR
+1 x Red LED
+2 x Resistor (100 ohm)
+1 x Breadboard
+1 x Micro-B USB Cable
+*/
+
+/* Pins for LED and LDR */
+const int led = D0;
+const int ldr = A0;
+
+int ldrValue;
+
+void setup() {
+  Serial.begin(115200); //open the serial port at 115200 bps
+}
+
+void loop() {
+  ldrValue = analogRead(ldr); //read LDR value
+  ldrValue = map(ldrValue, 4095, 0, 0, 255); //map value from 13 bit to 8 bit and reverse it
+  Serial.println(ldrValue); //show the value in Serial Monitor
+  analogWrite(led, ldrValue); //adjust the brightness of the LED
+  delay(100);
+}
